a6987e9e29a6fcfa5fcb08d5ada670a2af9e07c7
Joey199-sudo <94124588+Joey199-sudo@users.noreply.github.com>
2024-12-21 12:58:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './zebras.jpg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className='App-logo img-fluid' 
+        alt='logo' />
+
+      </header>
+      Hello Joey
+    </div>
+  );
+}
+
+App;
